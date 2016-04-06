d85c87402367b8341637ed9622b4b06688e00490
panshen <790928421@qq.com>
2016-04-06 15:08:38 +0800

    ooo

--- app/src/main/java/presenter/mmainpresenter.java
+++ app/src/main/java/presenter/mmainpresenter.java
@@ -1,5 +1,7 @@
 package presenter;
 
+import android.os.Bundle;
+
 import com.jude.beam.bijection.Presenter;
 
 import view.activity.mmainactivity;
@@ -9,4 +11,11 @@ import view.activity.mmainactivity;
  */
 public class mmainpresenter  extends Presenter<mmainactivity> {
 
+    @Override
+    protected void onCreateView(mmainactivity view) {
+        super.onCreateView(view);
+    }
+    public void changetextpersenter(){
+        getView().changeview();
+    }
 }
--- app/src/main/java/view/activity/mmainactivity.java
+++ app/src/main/java/view/activity/mmainactivity.java
@@ -1,19 +1,36 @@
 package view.activity;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.widget.TextView;
 
 import com.jude.beam.bijection.RequiresPresenter;
 import com.jude.beam.expansion.BeamBaseActivity;
+import com.panshen.ajiandan.ajiandan.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import presenter.mmainpresenter;
 
 @RequiresPresenter(mmainpresenter.class)
 public class mmainactivity extends BeamBaseActivity<mmainpresenter> {
+    @Bind(R.id.text_view)
+    TextView tv;
 
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-            super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout);
+        ButterKnife.bind(this);
+        tv.setText("ttttttt");
     }
 
+    public void changeview() {
+        tv.setText("aaaaaa");
+    }
+
+    @OnClick(R.id.text_view)
+    void submit() {
+        getPresenter().changetextpersenter();
+    }
 }
